d0d136f46105e3475b1806edac9cdb99a3e2ced9
Tyler Melanson <tylermelanson28@gmail.com>
2025-11-13 08:52:50 -0500

    StockLot class for managing stock shares

---
+++ StockLot.java
@@ -0,0 +1,28 @@
+public class StockLot
+{
+    private int shares;
+    private double buyPrice; 
+    
+    public StockLot(int s, double p)
+    {
+        shares = s;
+        buyPrice = p;
+    }
+    
+    public int getShares()
+    {
+        return shares;
+    }
+    
+    public double getBuyPrice()
+    {
+        return buyPrice;
+    }
+    
+    //Used to take out some of the shares from the lot
+    public void setShares(int newShares)
+    {
+    if (newShares < shares)
+    shares = newShares;
+    }
+}
